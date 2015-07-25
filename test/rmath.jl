d86dd9692aad1cd7dc0d86ef5761a90ef8c19a12
Dahua Lin <lindahua@gmail.com>
2015-07-26 00:23:36 +0800

    a new way to test rmath

--- test/rmath.jl
+++ test/rmath.jl
@@ -1,7 +1,9 @@
 using StatsFuns
 using Base.Test
 
-function check_rmath(fname, params, aname, a, v, rv, isprob, rtol)
+function check_rmath(fname, statsfun, rmathfun, params, aname, a, isprob, rtol)
+    v = statsfun(params..., a)
+    rv = rmathfun(params..., a)
     if isprob
         rd = abs(v / rv - 1.0)
         if rd > rtol
@@ -20,60 +22,87 @@ function check_rmath(fname, params, aname, a, v, rv, isprob, rtol)
     end
 end
 
-macro generate_rmath_compfun(basename)
-    # function symbols
-    compfun = symbol(string("rmathcomp_", basename))
+get_statsfun(fname) = eval(symbol(fname))
+get_rmathfun(fname) = eval(parse(string("Rmath.", fname)))
 
-    pdf     = symbol(string(basename, "pdf"))
-    logpdf  = symbol(string(basename, "logpdf"))
-    cdf     = symbol(string(basename, "cdf"))
-    ccdf    = symbol(string(basename, "ccdf"))
-    logcdf  = symbol(string(basename, "logcdf"))
-    logccdf = symbol(string(basename, "logccdf"))
+function rmathcomp(basename, params, X::AbstractArray, rtol=1.0e-12)
+    pdf     = string(basename, "pdf")
+    logpdf  = string(basename, "logpdf")
+    cdf     = string(basename, "cdf")
+    ccdf    = string(basename, "ccdf")
+    logcdf  = string(basename, "logcdf")
+    logccdf = string(basename, "logccdf")
 
-    invcdf     = symbol(string(basename, "invcdf"))
-    invccdf    = symbol(string(basename, "invccdf"))
-    invlogcdf  = symbol(string(basename, "invlogcdf"))
-    invlogccdf = symbol(string(basename, "invlogccdf"))
+    stats_pdf = get_statsfun(pdf)
+    stats_logpdf = get_statsfun(logpdf)
 
-    esc(quote
-        function $(compfun){N}(params::NTuple{N,Real}, X::AbstractArray, rtol=1.0e-12)
-            for i = 1:length(X)
-                x = X[i]
-                check_rmath($(string(pdf)), params, "x", x,
-                    $(pdf)(params..., x), Rmath.$(pdf)(params..., x), true, rtol)
-                check_rmath($(string(logpdf)), params, "x", x,
-                    $(logpdf)(params..., x), Rmath.$(logpdf)(params..., x), false, rtol)
+    rmath_pdf = get_statsfun(pdf)
+    rmath_logpdf = get_statsfun(logpdf)
 
-                check_rmath($(string(cdf)), params, "x", x,
-                    $(cdf)(params..., x), Rmath.$(cdf)(params..., x), true, rtol)
-                check_rmath($(string(ccdf)), params, "x", x,
-                    $(ccdf)(params..., x), Rmath.$(ccdf)(params..., x), true, rtol)
-                check_rmath($(string(logcdf)), params, "x", x,
-                    $(logcdf)(params..., x), Rmath.$(logcdf)(params..., x), false, rtol)
-                check_rmath($(string(logccdf)), params, "x", x,
-                    $(logccdf)(params..., x), Rmath.$(logccdf)(params..., x), true, rtol)
-
-                p = Rmath.$(cdf)(params..., x)
-                cp = Rmath.$(ccdf)(params..., x)
-                lp = log(p)
-                lcp = log(cp)
-
-                check_rmath($(string(invcdf)), params, "p", p,
-                    $(invcdf)(params..., p), Rmath.$(invcdf)(params..., p), false, rtol)
-                check_rmath($(string(invccdf)), params, "p", cp,
-                    $(invccdf)(params..., cp), Rmath.$(invccdf)(params..., cp), false, rtol)
-                check_rmath($(string(invlogcdf)), params, "lp", lp,
-                    $(invlogcdf)(params..., lp), Rmath.$(invlogcdf)(params..., lp), false, rtol)
-                check_rmath($(string(invlogccdf)), params, "lp", lcp,
-                    $(invlogccdf)(params..., lcp), Rmath.$(invlogccdf)(params..., lcp), false, rtol)
-            end
-        end
-    end)
+    for i = 1:length(X)
+        x = X[i]
+        check_rmath(pdf, stats_pdf, rmath_pdf,
+            params, "x", x, true, rtol)
+        check_rmath(logpdf, stats_logpdf, rmath_logpdf,
+            params, "x", x, false, rtol)
+    end
 end
 
-@generate_rmath_compfun norm
 
-rmathcomp_norm((0.0, 1.0), -6.0:0.01:6.0)
-rmathcomp_norm((2.0, 1.0), -3.0:0.01:7.0)
-rmathcomp_norm((0.0, 0.5), -3.0:0.01:3.0)
+# macro generate_rmath_compfun(basename)
+#     # function symbols
+#     compfun = symbol(string("rmathcomp_", basename))
+#
+#     pdf     = symbol(string(basename, "pdf"))
+#     logpdf  = symbol(string(basename, "logpdf"))
+#     cdf     = symbol(string(basename, "cdf"))
+#     ccdf    = symbol(string(basename, "ccdf"))
+#     logcdf  = symbol(string(basename, "logcdf"))
+#     logccdf = symbol(string(basename, "logccdf"))
+#
+#     invcdf     = symbol(string(basename, "invcdf"))
+#     invccdf    = symbol(string(basename, "invccdf"))
+#     invlogcdf  = symbol(string(basename, "invlogcdf"))
+#     invlogccdf = symbol(string(basename, "invlogccdf"))
+#
+#     esc(quote
+#         function $(compfun){N}(params::NTuple{N,Real}, X::AbstractArray, rtol=1.0e-12)
+#             for i = 1:length(X)
+#                 x = X[i]
+#                 check_rmath($(string(pdf)), params, "x", x,
+#                     $(pdf)(params..., x), Rmath.$(pdf)(params..., x), true, rtol)
+#                 check_rmath($(string(logpdf)), params, "x", x,
+#                     $(logpdf)(params..., x), Rmath.$(logpdf)(params..., x), false, rtol)
+#
+#                 check_rmath($(string(cdf)), params, "x", x,
+#                     $(cdf)(params..., x), Rmath.$(cdf)(params..., x), true, rtol)
+#                 check_rmath($(string(ccdf)), params, "x", x,
+#                     $(ccdf)(params..., x), Rmath.$(ccdf)(params..., x), true, rtol)
+#                 check_rmath($(string(logcdf)), params, "x", x,
+#                     $(logcdf)(params..., x), Rmath.$(logcdf)(params..., x), false, rtol)
+#                 check_rmath($(string(logccdf)), params, "x", x,
+#                     $(logccdf)(params..., x), Rmath.$(logccdf)(params..., x), true, rtol)
+#
+#                 p = Rmath.$(cdf)(params..., x)
+#                 cp = Rmath.$(ccdf)(params..., x)
+#                 lp = log(p)
+#                 lcp = log(cp)
+#
+#                 check_rmath($(string(invcdf)), params, "p", p,
+#                     $(invcdf)(params..., p), Rmath.$(invcdf)(params..., p), false, rtol)
+#                 check_rmath($(string(invccdf)), params, "p", cp,
+#                     $(invccdf)(params..., cp), Rmath.$(invccdf)(params..., cp), false, rtol)
+#                 check_rmath($(string(invlogcdf)), params, "lp", lp,
+#                     $(invlogcdf)(params..., lp), Rmath.$(invlogcdf)(params..., lp), false, rtol)
+#                 check_rmath($(string(invlogccdf)), params, "lp", lcp,
+#                     $(invlogccdf)(params..., lcp), Rmath.$(invlogccdf)(params..., lcp), false, rtol)
+#             end
+#         end
+#     end)
+# end
+#
+# @generate_rmath_compfun norm
+
+rmathcomp("norm", (0.0, 1.0), -6.0:0.01:6.0)
+rmathcomp("norm", (2.0, 1.0), -3.0:0.01:7.0)
+rmathcomp("norm", (0.0, 0.5), -3.0:0.01:3.0)
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,9 +2,9 @@ tests = ["basicfuns", "rmath"]
 
 for t in tests
     to_test = true
-    if VERSION < v"0.4.0-dev"
-        to_test = false
-    end
+    # if VERSION < v"0.4.0-dev"
+    #     to_test = false
+    # end
 
     if to_test
         fp = "$t.jl"
