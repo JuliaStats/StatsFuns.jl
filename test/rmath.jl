5f18dc18cf1df7d6e8e74b5c208d1ca37cc9d06c
Dahua Lin <lindahua@gmail.com>
2015-07-25 23:43:40 +0800

    completely disable generate_math_compfun

--- test/rmath.jl
+++ test/rmath.jl
@@ -45,15 +45,15 @@ macro generate_rmath_compfun(basename)
         function $(compfun){N}(params::NTuple{N,Real}, X::AbstractArray, rtol=1.0e-12)
             for i = 1:length(X)
                 x = X[i]
-                check_rmath($pdfname, params, "x", x,
-                    $(pdf)(params..., x), Rmath.$(pdf)(params..., x), true, rtol)
-                check_rmath($logpdfname, params, "x", x,
-                    $(logpdf)(params..., x), Rmath.$(logpdf)(params..., x), false, rtol)
-
-                check_rmath($cdfname, params, "x", x,
-                    $(cdf)(params..., x), Rmath.$(cdf)(params..., x), true, rtol)
-                check_rmath($ccdfname, params, "x", x,
-                    $(ccdf)(params..., x), Rmath.$(ccdf)(params..., x), true, rtol)
+                # check_rmath($pdfname, params, "x", x,
+                #     $(pdf)(params..., x), Rmath.$(pdf)(params..., x), true, rtol)
+                # check_rmath($logpdfname, params, "x", x,
+                #     $(logpdf)(params..., x), Rmath.$(logpdf)(params..., x), false, rtol)
+                #
+                # check_rmath($cdfname, params, "x", x,
+                #     $(cdf)(params..., x), Rmath.$(cdf)(params..., x), true, rtol)
+                # check_rmath($ccdfname, params, "x", x,
+                #     $(ccdf)(params..., x), Rmath.$(ccdf)(params..., x), true, rtol)
                 # check_rmath($(string(logcdf)), params, "x", x,
                 #     $(logcdf)(params..., x), Rmath.$(logcdf)(params..., x), false, rtol)
                 # check_rmath($(string(logccdf)), params, "x", x,
