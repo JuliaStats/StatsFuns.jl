0f5154e740c320e5a616e972ff570225123e6fe9
Kai Xu <xukai921110@gmail.com>
2016-11-07 22:32:35 +0000

    Replace beta and gamma with lbeta and lgamma

--- src/distrs/gamma.jl
+++ src/distrs/gamma.jl
@@ -15,4 +15,4 @@ import .RFunctions:
 gammapdf(k::Real, θ::Real, x::Number) = 1 / (gamma(k) * θ^k) * x^(k - 1) * exp(-x / θ)
 
 # logpdf
-gammalogpdf(k::Real, θ::Real, x::Number) = -log(gamma(k)) - k * log(θ) + (k - 1) * log(x) - x / θ
+gammalogpdf(k::Real, θ::Real, x::Number) = -lgamma(k) - k * log(θ) + (k - 1) * log(x) - x / θ
