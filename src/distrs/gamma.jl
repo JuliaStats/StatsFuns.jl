392259ec60d8ce9b4005848a963d329731cc1a71
Kai Xu <xukai921110@gmail.com>
2016-11-08 19:40:25 +0000

    Use Number to keep consistent with existing Normal functions

--- src/distrs/gamma.jl
+++ src/distrs/gamma.jl
@@ -13,7 +13,7 @@ import .RFunctions:
     gammainvlogccdf
 
 # pdf for numbers with generic types
-gammapdf(k::Real, θ::Real, x::Real) = 1 / (gamma(k) * θ^k) * x^(k - 1) * exp(-x / θ)
+gammapdf(k::Real, θ::Real, x::Number) = 1 / (gamma(k) * θ^k) * x^(k - 1) * exp(-x / θ)
 
 # logpdf for numbers with generic types
-gammalogpdf(k::Real, θ::Real, x::Real) = -lgamma(k) - k * log(θ) + (k - 1) * log(x) - x / θ
+gammalogpdf(k::Real, θ::Real, x::Number) = -lgamma(k) - k * log(θ) + (k - 1) * log(x) - x / θ
