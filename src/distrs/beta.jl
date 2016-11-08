54360f046fb7f00bcdcbd9131fac56aa10a433c9
Kai Xu <xukai921110@gmail.com>
2016-11-08 19:40:35 +0000

    Use Number to keep consistent with existing Normal functions

--- src/distrs/beta.jl
+++ src/distrs/beta.jl
@@ -13,7 +13,7 @@ import .RFunctions:
     betainvlogccdf
 
 # pdf for numbers with generic types
-betapdf(α::Real, β::Real, x::Real) = x^(α - 1) * (1 - x)^(β - 1) / beta(α, β)
+betapdf(α::Real, β::Real, x::Number) = x^(α - 1) * (1 - x)^(β - 1) / beta(α, β)
 
 # logpdf for numbers with generic types
-betalogpdf(α::Real, β::Real, x::Real) = (α - 1) * log(x) + (β - 1) * log1p(-x) - lbeta(α, β)
+betalogpdf(α::Real, β::Real, x::Number) = (α - 1) * log(x) + (β - 1) * log1p(-x) - lbeta(α, β)
