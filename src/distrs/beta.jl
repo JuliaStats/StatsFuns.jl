16172469465b94a3a53895630f2a7220891e0f37
Kai Xu <xukai921110@gmail.com>
2016-11-08 19:01:06 +0000

    Add some comments

--- src/distrs/beta.jl
+++ src/distrs/beta.jl
@@ -12,8 +12,8 @@ import .RFunctions:
     betainvlogcdf,
     betainvlogccdf
 
-# pdf
+# pdf for numbers with generic types
 betapdf(α::Real, β::Real, x::Real) = x^(α - 1) * (1 - x)^(β - 1) / beta(α, β)
 
-# logpdf
+# logpdf for numbers with generic types
 betalogpdf(α::Real, β::Real, x::Real) = (α - 1) * log(x) + (β - 1) * log1p(-x) - lbeta(α, β)
--- src/distrs/gamma.jl
+++ src/distrs/gamma.jl
@@ -12,8 +12,8 @@ import .RFunctions:
     gammainvlogcdf,
     gammainvlogccdf
 
-# pdf
+# pdf for numbers with generic types
 gammapdf(k::Real, θ::Real, x::Real) = 1 / (gamma(k) * θ^k) * x^(k - 1) * exp(-x / θ)
 
-# logpdf
+# logpdf for numbers with generic types
 gammalogpdf(k::Real, θ::Real, x::Real) = -lgamma(k) - k * log(θ) + (k - 1) * log(x) - x / θ
