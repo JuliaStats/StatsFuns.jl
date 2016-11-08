9c22ba3f92fac9323b1ce1d41739311a06bc9a89
Kai Xu <xukai921110@gmail.com>
2016-11-08 18:39:53 +0000

    Improve precision

--- src/distrs/beta.jl
+++ src/distrs/beta.jl
@@ -14,4 +14,4 @@ import .RFunctions:
 betapdf(α::Real, β::Real, x::Number) = x^(α - 1) * (1 - x)^(β - 1) / beta(α, β)
 
 # logpdf
-betalogpdf(α::Real, β::Real, x::Number) = (α - 1) * log(x) + (β - 1) * log(1 - x) - lbeta(α, β)
+betalogpdf(α::Real, β::Real, x::Number) = (α - 1) * log(x) + (β - 1) * log1p(-x) - lbeta(α, β)
