667d315ea4391dda3ae7b2944e8977e5c2d3b8cf
Kai Xu <xukai921110@gmail.com>
2016-12-05 22:13:33 +0000

    Add generic support callback for student's T dist

--- src/distrs/tdist.jl
+++ src/distrs/tdist.jl
@@ -11,3 +11,9 @@ import .RFunctions:
     tdistinvccdf,
     tdistinvlogcdf,
     tdistinvlogccdf
+
+# pdf for numbers with generic types
+tdistpdf(ν::Real, x::Number) = gamma(0.5 * (ν + 1)) / (sqrt(ν * pi) * gamma(0.5 * ν)) * (1 + x^2 / ν)^(-0.5 * (ν + 1))
+
+# logpdf for numbers with generic types
+tdistlogpdf(ν::Real, x::Number) = lgamma(0.5 * (ν + 1)) - 0.5 * log(ν * pi) - lgamma(0.5 * ν) + (-0.5 * (ν + 1)) * log(1 + x^2 / ν)
