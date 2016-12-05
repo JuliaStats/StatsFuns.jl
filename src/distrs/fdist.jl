88cea7a76b9074c1b5bcab5457a3729fe3975861
Kai Xu <xukai921110@gmail.com>
2016-12-05 21:46:27 +0000

    Add generic support callback for F dist

--- src/distrs/fdist.jl
+++ src/distrs/fdist.jl
@@ -11,3 +11,9 @@ import .RFunctions:
     fdistinvccdf,
     fdistinvlogcdf,
     fdistinvlogccdf
+
+# pdf for numbers with generic types
+fdistpdf(d1::Real, d2::Real, x::Number) = sqrt((d1 * x)^d1 * d2^d2 / (d1 * x + d2)^(d1 + d2)) / (x * beta(0.5 * d1, 0.5 * d2))
+
+# logpdf for numbers with generic types
+fdistlogpdf(d1::Real, d2::Real, x::Number) = 0.5 * (d1 * log(d1 * x) + d2 * log(d2) - (d1 + d2) * log(d1 * x + d2)) - log(x) - lbeta(0.5 * d1, 0.5 * d2)
