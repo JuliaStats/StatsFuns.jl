df9a78e6bf74491607b4b0b8791445daac1f6c4c
Kai Xu <xukai921110@gmail.com>
2016-12-05 21:07:37 +0000

    Add generic support callback for Chisq

--- src/distrs/chisq.jl
+++ src/distrs/chisq.jl
@@ -11,3 +11,15 @@ import .RFunctions:
     chisqinvccdf,
     chisqinvlogcdf,
     chisqinvlogccdf
+
+# pdf for numbers with generic types
+function chisqpdf(k::Real, x::Number)
+  hk = 0.5 * k  # hlaf k
+  1 / (2^(hk) * gamma(hk)) * x^(hk - 1) * exp(-0.5 * x)
+end
+
+# logpdf for numbers with generic types
+function chisqlogpdf(k::Real, x::Number)
+  hk = 0.5 * k  # hlaf k
+  -hk * log(2) - lgamma(hk) + (hk - 1) * log(x) - 0.5 * x
+end
