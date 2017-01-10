2ff9dd1430d7842499912edcfd5a2e85872f31f8
Kai Xu <xukai921110@gmail.com>
2017-01-10 17:01:01 +0000

    Improve code efficiency

--- src/distrs/chisq.jl
+++ src/distrs/chisq.jl
@@ -14,12 +14,12 @@ import .RFunctions:
 
 # pdf for numbers with generic types
 function chisqpdf(k::Real, x::Number)
-  hk = 0.5 * k  # hlaf k
+  hk = k / 2  # hlaf k
   1 / (2^(hk) * gamma(hk)) * x^(hk - 1) * exp(-0.5 * x)
 end
 
 # logpdf for numbers with generic types
 function chisqlogpdf(k::Real, x::Number)
-  hk = 0.5 * k  # hlaf k
-  -hk * log(2) - lgamma(hk) + (hk - 1) * log(x) - 0.5 * x
+  hk = k / 2  # hlaf k
+  -hk * log(oftype(hk, 2)) - lgamma(hk) + (hk - 1) * log(x) - 0.5 * x
 end
