dd1a27f3b931bf68aabb315eea4dc47d76161ab0
Kai Xu <xukai921110@gmail.com>
2017-01-11 10:19:13 +0000

    Change *0.5 to /2

--- src/distrs/tdist.jl
+++ src/distrs/tdist.jl
@@ -13,7 +13,7 @@ import .RFunctions:
     tdistinvlogccdf
 
 # pdf for numbers with generic types
-tdistpdf(ν::Real, x::Number) = gamma(0.5 * (ν + 1)) / (sqrt(ν * pi) * gamma(ν / 2)) * (1 + x^2 / ν)^(-(ν + 1) / 2)
+tdistpdf(ν::Real, x::Number) = gamma((ν + 1) / 2) / (sqrt(ν * pi) * gamma(ν / 2)) * (1 + x^2 / ν)^(-(ν + 1) / 2)
 
 # logpdf for numbers with generic types
 tdistlogpdf(ν::Real, x::Number) = lgamma((ν + 1) / 2) - log(ν * pi) / 2 - lgamma(ν / 2) + (-(ν + 1) / 2) * log(1 + x^2 / ν)
