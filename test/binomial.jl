bcec450c8bc66bf605fc1a59acd24e3b4620e21f
Unknown <adityapuranik97@gmail.com>
2018-03-04 18:47:52 +0530

    Test fix

--- test/binomial.jl
+++ test/binomial.jl
@@ -7,16 +7,19 @@ N = (1, 1, 8, 20, 20, 20, 150, 700, 900, 1000)::NTuple{10, Int64};
 P = (0.5, 0.7, 0.6, 0.34, 0.89, 0.53, 0.77, 0.98, 0.5, 0.29)::NTuple{10, Float64};
 
 println("\ttesting binompdf ...");
-
 for i = 1:length(N)
     for x = Int64(0):N[i]
-@test abs(binompdf(N[i], P[i], x)/Rmath.dbinom(x, N[i], P[i]) - 1.0) < 2.23e-14
+    if isnan(abs(binompdf(N[i], P[i], x) - Rmath.dbinom(x, N[i], P[i]))/binompdf(N[i], P[i], x)) == false
+    @test abs(binompdf(N[i], P[i], x) - Rmath.dbinom(x, N[i], P[i])) < 1e-15
+    else
+    @test binompdf(N[i], P[i], x) == Rmath.dbinom(x, N[i], P[i])
     end
+end
 end
 
 println("\ttesting binomlogpdf ...");
 for i = 1:length(N)
     for x = Int64(0):N[i]
-    @test  abs(binomlogpdf(N[i], P[i], x)/(Rmath.dbinom(x, N[i], P[i], true) - 1.0) < 2.23e-14
+    @test  abs(binomlogpdf(N[i], P[i], x) - Rmath.dbinom(x, N[i], P[i], true)/binomlogpdf(N[i], P[i], x) < 2.24e-14)
     end
 end
