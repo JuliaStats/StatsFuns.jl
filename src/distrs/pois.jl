2a3b076fd1681c1bceb078153e55fa9d342585bb
Douglas Bates <dmbates@gmail.com>
2018-03-06 14:14:39 -0600

    Slightly cleaner way of determining type

--- src/distrs/pois.jl
+++ src/distrs/pois.jl
@@ -17,13 +17,11 @@ poispdf(λ::Real, x::Real) = exp(poislogpdf(λ, x))
 
 function poislogpdf(λ::Real, x::Real)
     (λ ≥ 0 && x ≥ 0) || throw(ArgumentError("λ = $λ and x = $x must both be ≥ 0"))
-    λ = float(λ)
-    nInf = oftype(λ, -Inf) * one(x)
-    isinteger(x) || return nInf
-    iszero(λ) && return (iszero(x) ? zero(λ) : nInf) * one(x)
-    iszero(x) && return -λ * one(x)
-    x * log(λ) - λ - lgamma(one(λ) * (x + one(x)))
+    T = typeof(float(λ) * x)
+    λ = T(λ)
+    x = T(x)
+    isinteger(x) || return T(-Inf)
+    iszero(λ) && return iszero(x) ? zero(T) : T(-Inf)
+    iszero(x) && return -λ
+    x * log(λ) - λ - lgamma(x + one(x))
 end
-
-# The peculiar argument in `lgamma(one(λ) * (x + one(x)))` is to retain precision
-# when `λ` is a BigFloat but also allow for `x` to be a Dual number.
