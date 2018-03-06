94d12a497955261ba195d08fdd372759999b7309
Douglas Bates <dmbates@gmail.com>
2018-03-06 14:35:41 -0600

    Solution suggested by Andreas

--- src/distrs/pois.jl
+++ src/distrs/pois.jl
@@ -15,13 +15,6 @@ import .RFunctions:
 # generic versions
 poispdf(λ::Real, x::Real) = exp(poislogpdf(λ, x))
 
-function poislogpdf(λ::Real, x::Real)
-    (λ ≥ 0 && x ≥ 0) || throw(ArgumentError("λ = $λ and x = $x must both be ≥ 0"))
-    T = typeof(float(λ) * x)
-    λ = T(λ)
-    x = T(x)
-    isinteger(x) || return T(-Inf)
-    iszero(λ) && return iszero(x) ? zero(T) : T(-Inf)
-    iszero(x) && return -λ
-    x * log(λ) - λ - lgamma(x + one(x))
-end
+poislogpdf(λ::T, x::T) where {T <: Real} = x * log(λ) - λ - lgamma(x + 1)
+
+poislogpdf(λ::Number, x::Number) = poislogpdf(promote(λ, x)...)
