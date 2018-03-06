79ede8341a01679a3ac07c83212a7b6950bf0f83
Douglas Bates <dmbates@gmail.com>
2018-03-06 12:11:07 -0600

    Generic versions of poislogpdf and tests

--- src/distrs/pois.jl
+++ src/distrs/pois.jl
@@ -11,3 +11,16 @@ import .RFunctions:
     poisinvccdf,
     poisinvlogcdf,
     poisinvlogccdf
+
+# generic versions
+poispdf(λ::Real, x::Real) = exp(poislogpdf(λ, x))
+
+function poislogpdf(λ::Real, x::Real)
+    (λ ≥ 0 && x ≥ 0) || throw(ArgumentError("λ = $λ and x = $x must both be ≥ 0"))
+    λ = float(λ)
+    nInf = oftype(λ, -Inf)
+    isinteger(x) || return nInf
+    iszero(λ) && return iszero(x) ? zero(λ) : nInf
+    iszero(x) && return -λ
+    x * log(λ) - λ - lgamma(oftype(λ, x + one(x)))
+end
