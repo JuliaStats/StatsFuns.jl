755d8b879fba192e0ca93e76cc5ec95e505642b3
Douglas Bates <dmbates@gmail.com>
2018-03-07 13:39:09 -0600

    Add a generic Poisson pdf and logpdf (#35)
    * Spelling
    
    * Generic versions of poislogpdf and tests
    
    * Allow for Dual numbers as second argument
    
    * Add missing newline
    
    * Slightly cleaner way of determining type
    
    * Solution suggested by Andreas
    
    * Handle the case of λ = 0 separately
    
    * Use xlogy in poislogpdf
    
    * Another valuable suggestion from Andreas

--- src/distrs/chisq.jl
+++ src/distrs/chisq.jl
@@ -14,12 +14,12 @@ import .RFunctions:
 
 # pdf for numbers with generic types
 function chisqpdf(k::Real, x::Number)
-  hk = k / 2  # hlaf k
+  hk = k / 2  # half k
   1 / (2^(hk) * gamma(hk)) * x^(hk - 1) * exp(-x / 2)
 end
 
 # logpdf for numbers with generic types
 function chisqlogpdf(k::Real, x::Number)
-  hk = k / 2  # hlaf k
+  hk = k / 2  # half k
   -hk * log(oftype(hk, 2)) - lgamma(hk) + (hk - 1) * log(x) - x / 2
 end
--- src/distrs/pois.jl
+++ src/distrs/pois.jl
@@ -11,3 +11,10 @@ import .RFunctions:
     poisinvccdf,
     poisinvlogcdf,
     poisinvlogccdf
+
+# generic versions
+poispdf(λ::Real, x::Real) = exp(poislogpdf(λ, x))
+
+poislogpdf(λ::T, x::T) where {T <: Real} = xlogy(x, λ) - λ - lgamma(x + 1)
+
+poislogpdf(λ::Number, x::Number) = poislogpdf(promote(float(λ), x)...)
