6c183451bf5ce906f96095c7e2f2bb2af279b024
Simon Byrne <simonbyrne@gmail.com>
2018-03-09 10:13:15 -0800

    Add link to DLMF.

--- src/misc.jl
+++ src/misc.jl
@@ -3,7 +3,7 @@
 """
     logmvgamma(p::Int, a::Real)
 
-Return the logarithm of [multivariate gamma function](https://en.wikipedia.org/wiki/Multivariate_gamma_function)
+Return the logarithm of [multivariate gamma function](https://en.wikipedia.org/wiki/Multivariate_gamma_function) ([DLMF 35.3.1](https://dlmf.nist.gov/35.3.1)).
 """
 function logmvgamma(p::Int, a::Real)
     # NOTE: one(a) factors are here to prevent unnecessary promotion of Float32
