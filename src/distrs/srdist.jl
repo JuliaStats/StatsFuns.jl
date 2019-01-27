2487075478b5631bf2492ca9afbb4214339315da
Nicholas Bauer <nicholasbauer@outlook.com>
2019-01-27 11:45:44 -0500

    Simplified pdf code

--- src/distrs/srdist.jl
+++ src/distrs/srdist.jl
@@ -18,13 +18,11 @@ function srdistpdf(ν::Real, k::Real, x::Number)
     Φ(x) = (1+erf(x / √2)) / 2
     ϕ(x) = derivative(Φ, x)
     function outer(y)
-        function inner(u)
-            return ϕ(u) * ϕ(u - x*y) * (Φ(u) - Φ(u - x*y))^(k-2)
-        end
-        inner_part = quadgk(inner, -Inf, Inf)[1]
+        inner(u) = ϕ(u) * ϕ(u - x*y) * (Φ(u) - Φ(u - x*y))^(k-2)
+        inner_part = quadgk(inner, -Inf, Inf) |> first
         return inner_part * y^ν * ϕ(y*√ν)
     end
-    integral = quadgk(outer, 0.0, Inf)[1]
+    integral = quadgk(outer, 0.0, Inf) |> first
     return integral * (√(2π) * k * (k-1) * ν^(ν/2)) / (gamma(ν/2) * 2^(ν/2 - 1))
 end
 
