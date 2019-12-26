0f1ec23d6c59f0160d4c8731f6dc379f77357f28
Eric Proffitt <eric.ai@icloud.com>
2019-12-26 18:27:55 -0500

    fix log problems
    using `log1p` with `logbeta` results in slight numerical inaccuracies, so changed to using all `loggamma`.
    
    `xlogy` needs to return zero(type(y)) in ternary expression, otherwise if x < 0 it throws complex result error.

--- src/distrs/binom.jl
+++ src/distrs/binom.jl
@@ -16,4 +16,12 @@ import .RFunctions:
 binompdf(n::Real, p::Real, k::Real) = exp(binomlogpdf(n, p, k))
 
 # logpdf for numbers with generic types
-binomlogpdf(n::Real, p::Real, k::Real) = -log1p(n) - logbeta(n - k + 1, k + 1) + k * log(p) + (n - k) * log1p(-p)
+function binomlogpdf(n::Real, p::Real, k::Real)
+    if k > n
+        x = -Inf 
+    else
+        x = loggamma(n + 1) - loggamma(k + 1) - loggamma(n - k + 1) + xlogy(k, p) + xlogy(n - k, 1 - p)
+    end
+    
+    return x
+end
