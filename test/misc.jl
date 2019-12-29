d77aaaa80254f1c696ea0a12653e5e9d48fd0e1c
Eric Proffitt <eric.ai@icloud.com>
2019-12-29 14:01:43 -0500

    cleaned up test

--- test/misc.jl
+++ test/misc.jl
@@ -62,7 +62,6 @@ end
         for p in push!(rand(50), [0, 1]...)
             for n in rand(0:50)
                 for k in push!(range(-50:50), randn(10)...)
-                    true
                     @test binomlogpdf(n, Float32(p), k) ≈ log(pdf(Binomial(n, p), k))
                 end
             end
