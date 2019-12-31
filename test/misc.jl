f14bc933ba36c3697eea3eab99f7dc06ade3bda1
Eric Proffitt <eric.ai@icloud.com>
2019-12-31 13:01:07 -0500

    Update test/misc.jl
    Co-Authored-By: Milan Bouchet-Valat <nalimilan@club.fr>

--- test/misc.jl
+++ test/misc.jl
@@ -59,7 +59,7 @@ end
 @testset "binomlogpdf" begin
     @testset "identity" begin
 
-        for p in push!(rand(50), [0, 1]...)
+        for p in [rand(50); 0; 1]
             for n in 0:50
                 for k in push!(collect(Any, -50:50), randn(10)...)
                     @test binomlogpdf(n, Float32(p), k) ≈ log(pdf(Binomial(n, p), k))
