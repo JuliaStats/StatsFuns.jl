510f5fca38b226b86875403cf2556f4923c45d74
Eric Proffitt <eric.ai@icloud.com>
2019-12-31 12:53:55 -0500

    Update src/distrs/binom.jl
    Co-Authored-By: Milan Bouchet-Valat <nalimilan@club.fr>

--- src/distrs/binom.jl
+++ src/distrs/binom.jl
@@ -18,7 +18,8 @@ binompdf(n::Real, p::Real, k::Real) = exp(binomlogpdf(n, p, k))
 # logpdf for numbers with generic types
 function binomlogpdf(n::Real, p::Real, k::Real)
     if isinteger(k) & (zero(k) <= k <= n)
-        x = loggamma(n + 1) - loggamma(k + 1) - loggamma(n - k + 1) + xlogy(k, p) + xlogy(n - k, 1 - p)
+        x = loggamma(n + 1) - loggamma(k + 1) - loggamma(n - k + 1) +
+            xlogy(k, p) + xlogy(n - k, 1 - p)
     else
         x = -Inf
     end
