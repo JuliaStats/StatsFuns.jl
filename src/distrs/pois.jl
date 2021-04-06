199ea180194d083115caf46a1e24bfb643013a09
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2021-04-06 20:10:21 +0200

    added Julia implementations of poislogcdf and poiscdf

--- src/distrs/pois.jl
+++ src/distrs/pois.jl
@@ -19,6 +19,14 @@ poislogpdf(λ::T, x::T) where {T <: Real} = xlogy(x, λ) - λ - loggamma(x + 1)
 
 poislogpdf(λ::Number, x::Number) = poislogpdf(promote(float(λ), x)...)
 
+poislogpdf(λ::Number, x::Number) = poislogpdf(promote(float(λ), x)...)
+
+function poislogcdf(λ::Number, x::Number)
+    return SpecialFunctions.loggamma(Int(x) + 1, λ) - SpecialFunctions.logfactorial(Int(x))
+end
+
+poiscdf(λ::Number, x::Number) = exp(poislogcdf(λ, x))
+
 #=
 function poislogpdf(λ::Union{Float32,Float64}, x::Union{Float64,Float32,Integer})
     if iszero(λ)
