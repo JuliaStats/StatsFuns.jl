0bae7bfb527b6d77f02666d0691d810a3eb24df2
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2021-04-07 14:39:56 +0200

    should not dispatch correctly for diffrules

--- src/distrs/pois.jl
+++ src/distrs/pois.jl
@@ -19,12 +19,15 @@ poislogpdf(λ::T, x::T) where {T <: Real} = xlogy(x, λ) - λ - loggamma(x + 1)
 
 poislogpdf(λ::Number, x::Number) = poislogpdf(promote(float(λ), x)...)
 
-function poislogcdf(λ::Number, x::Number)
+function poislogcdf(λ::T, x::T) where {T<:Real}
     return loggamma(floor(x + 1), float(λ)) - logfactorial(floor(x))
 end
+poislogcdf(λ::Number, x::Number) = poislogcdf(promote(float(λ), x)...)
 
-poiscdf(λ::Number, x::Number; precision=0) = last(gamma_inc(floor(x + 1), λ, precision))
-poisccdf(λ::Number, x::Number; precision=0) = first(gamma_inc(floor(x + 1), λ, precision))
+poiscdf(λ::T, x::T; precision=0) where {T<:Real} = last(gamma_inc(floor(x + 1), λ, precision))
+poiscdf(λ::Number, x::Number; precision=0) = poiscdf(promote(float(λ), x)...)
+poisccdf(λ::T, x::T; precision=0) where {T<:Real} = first(gamma_inc(floor(x + 1), λ, precision))
+poisccdf(λ::Number, x::Number; precision=0) = poisccdf(promote(float(λ), x)...)
 
 #=
 function poislogpdf(λ::Union{Float32,Float64}, x::Union{Float64,Float32,Integer})
