fcaddeea31c0f07e0f2ce3e13db8d8cd856b97b6
Andreas Noack <andreas@noack.dk>
2021-05-19 11:25:41 +0200

    Add native implementions for Poisson's cdf-like functions

--- src/distrs/pois.jl
+++ src/distrs/pois.jl
@@ -3,18 +3,25 @@
 import .RFunctions:
     # poispdf,
     # poislogpdf,
-    poiscdf,
-    poisccdf,
-    poislogcdf,
-    poislogccdf,
+    # poiscdf,
+    # poisccdf,
+    # poislogcdf,
+    # poislogccdf,
     poisinvcdf,
     poisinvccdf,
     poisinvlogcdf,
     poisinvlogccdf
 
-# generic versions
 poispdf(λ::Real, x::Real) = exp(poislogpdf(λ, x))
 
 poislogpdf(λ::T, x::T) where {T <: Real} = xlogy(x, λ) - λ - loggamma(x + 1)
-
 poislogpdf(λ::Real, x::Real) = poislogpdf(promote(float(λ), x)...)
+
+# Just use the Gamma definitions
+poiscdf(λ::Real, x::Real) = gammaccdf(floor(x + 1), 1, λ)
+
+poisccdf(λ::Real, x::Real) = gammacdf(floor(x + 1), 1, λ)
+
+poislogcdf(λ::Real, x::Real) = gammalogccdf(floor(x + 1), 1, λ)
+
+poislogccdf(λ::Real, x::Real) = gammalogcdf(floor(x + 1), 1, λ)
