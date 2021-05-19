b454b0dd4b59c5e24de9a3d61feb8663d84e8b0f
Andreas Noack <andreas@noack.dk>
2021-05-19 10:08:31 +0200

    Use the beta definitions for binomial

--- src/distrs/binom.jl
+++ src/distrs/binom.jl
@@ -12,12 +12,9 @@ import .RFunctions:
     binominvlogcdf,
     binominvlogccdf
 
-# pdf for numbers with generic types
-binompdf(n::Real, p::Real, k::Real) = exp(binomlogpdf(n, p, k))
+binompdf(n::Real, p::Real, k::Real) = betapdf(k + 1, n - k + 1, p)/(n + 1)
 
-# logpdf for numbers with generic types
-binomlogpdf(n::T, p::T, k::T) where T<:Real = -log1p(n) - logbeta(n - k + 1, k + 1) + k * log(p) + (n - k) * log1p(-p)
-binomlogpdf(n::Real, p::Real, k::Real) = binomlogpdf(promote(n, p, k)...)
+binomlogpdf(n::Real, p::Real, k::Real) = betalogpdf(k + 1, n - k + 1, p) - log(n + 1)
 
 binomcdf(n::Real, p::Real, k::Real) = betaccdf(k + 1, n - k, p)
 
