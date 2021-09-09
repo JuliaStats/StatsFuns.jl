66bb491d18e69be543155d3ef7b0e637d2f93736
David Widmann <dev+github@devmotion.de>
2021-09-10 00:22:59 +0200

    Add more tests

--- src/distrs/binom.jl
+++ src/distrs/binom.jl
@@ -18,6 +18,7 @@ binompdf(n::Real, p::Real, k::Real) = exp(binomlogpdf(n, p, k))
 
 binomlogpdf(n::Real, p::Real, k::Real) = binomlogpdf(promote(n, p, k)...)
 function binomlogpdf(n::T, p::T, k::T) where {T<:Real}
-    val = betalogpdf(k + 1, n - k + 1, p) - log(n + 1)
-    return 0 <= k <= n && isinteger(n) && isinteger(k) ? val : oftype(val, -Inf)
+    m = clamp(k, 0, n)
+    val = betalogpdf(m + 1, n - m + 1, p) - log(n + 1)
+    return 0 <= k <= n && isinteger(k) ? val : oftype(val, -Inf)
 end
--- src/distrs/pois.jl
+++ src/distrs/pois.jl
@@ -16,4 +16,7 @@ using .RFunctions:
 poispdf(λ::Real, x::Real) = exp(poislogpdf(λ, x))
 
 poislogpdf(λ::Real, x::Real) = poislogpdf(promote(λ, x)...)
-poislogpdf(λ::T, x::T) where {T <: Real} = xlogy(x, λ) - λ - loggamma(x + 1)
+function poislogpdf(λ::T, x::T) where {T <: Real}
+    val = xlogy(x, λ) - λ - loggamma(x + 1)
+    return x >= 0 && isinteger(x) ? val : oftype(val, -Inf)
+end
