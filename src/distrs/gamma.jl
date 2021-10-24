b2ed6c9c057e9a0e93bc0785a49e94ec961b912e
Andreas Noack <andreas@noack.dk>
2021-10-24 15:02:32 +0200

    Rely on gamma_inc_inv promotion in gammainv(c)cdf

--- src/distrs/gamma.jl
+++ src/distrs/gamma.jl
@@ -64,10 +64,6 @@ end
 gammalogccdf(k::Real, θ::Real, x::Real)        = gammalogccdf(promote(float(k), θ, x)...)
 gammalogccdf(k::T, θ::T, x::T) where T         = throw(MethodError(gammalogccdf, (k, θ, x)))
 
-gammainvcdf(k::Float64, θ::Float64, p::Float64) = θ*gamma_inc_inv(k, p, 1 - p)
-gammainvcdf(k::Real, θ::Real, p::Real) = gammainvcdf(promote(float(k), θ, p)...)
-gammainvcdf(k::T, θ::T, p::T) where T = throw(MethodError(gammainvcdf, (k, θ, p)))
+gammainvcdf(k::Real, θ::Real, p::Real) = θ*gamma_inc_inv(k, p, 1 - p)
 
-gammainvccdf(k::Float64, θ::Float64, p::Float64) = θ*gamma_inc_inv(k, 1 - p, p)
-gammainvccdf(k::Real, θ::Real, p::Real) = gammainvccdf(promote(float(k), θ, p)...)
-gammainvccdf(k::T, θ::T, p::T) where T = throw(MethodError(gammainvccdf, (k, θ, p)))
+gammainvccdf(k::Real, θ::Real, p::Real) = θ*gamma_inc_inv(k, 1 - p, p)
