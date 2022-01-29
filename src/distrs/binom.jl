8cd5a766b12e31754dda1d4ff2776bbcbf709aab
Andreas Noack <andreas@noack.dk>
2022-01-30 00:07:42 +0100

    Handle arguments out of support in Binomial (log)(c)cdf and apply the floor function to make the results correct for non-integer arguments

--- src/distrs/binom.jl
+++ src/distrs/binom.jl
@@ -23,10 +23,10 @@ function binomlogpdf(n::T, p::T, k::T) where {T<:Real}
     return 0 <= k <= n && isinteger(k) ? val : oftype(val, -Inf)
 end
 
-binomcdf(n::Real, p::Real, k::Real) = betaccdf(k + 1, n - k, p)
+binomcdf(n::Real, p::Real, k::Real) = betaccdf(max(0, floor(k) + 1), max(0, n - floor(k)), p)
 
-binomccdf(n::Real, p::Real, k::Real) = betacdf(k + 1, n - k, p)
+binomccdf(n::Real, p::Real, k::Real) = betacdf(max(0, floor(k) + 1), max(0, n - floor(k)), p)
 
-binomlogcdf(n::Real, p::Real, k::Real) = betalogccdf(k + 1, n - k, p)
+binomlogcdf(n::Real, p::Real, k::Real) = betalogccdf(max(0, floor(k) + 1), max(0, n - floor(k)), p)
 
-binomlogccdf(n::Real, p::Real, k::Real) = betalogcdf(k + 1, n - k, p)
+binomlogccdf(n::Real, p::Real, k::Real) = betalogcdf(max(0, floor(k) + 1), max(0, n - floor(k)), p)
