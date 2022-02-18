28d732a17078c87e2fde43da97de8fae310600e0
Andreas Noack <andreas@noack.dk>
2022-02-18 12:04:50 +0100

    Use julia implementations for pdfs and some cdf-like functions (#113)
    * Use Julia implementations for (log)pdfs. Adjust Rmath tests to use
    testsets to give more informative errors.
    
    * Use Julia implementations for most cdf-like functions of the Chisq
    distribution.
    
    * Change Beta distribution to be using native implementations.
    
    * Switch binomial cdfs to use native implementations (but not the inverses)
    
    * Use native implementations for most cdf like gamma functions
    
    * Just use the gamma defitions for the chisq
    
    * Use the beta definitions for binomial
    
    * Add native implementions for Poisson's cdf-like functions
    
    * Rely on SpecialFunctions promotion handling in beta functions
    
    * Rely on gamma_inc_inv promotion in gammainv(c)cdf
    
    * Apply suggestions from code review
    
    * Add compat for HypergeometricFunctions
    
    * Handle negative arguments in Poisson (log)(c)cdf
    
    * Handle negative x in Gamma (log)(c)cdf
    
    * Handle arguments outside support for Beta and Fdist (log)(c)cdf
    
    * Fix Fdist quantile functions
    
    * Handle arguments out of support in Binomial (log)(c)cdf and apply
    the floor function to make the results correct for non-integer arguments
    
    * Avoid accidental promotion to Float64 in fdist(log)(c)cdf
    
    * Fix typo in "x" calculation for fdist(log)(c)cdf
    
    * Drop redundant low tolerance tests for Beta(1000, 2). We are now
    generally more accurate than Rmath so failure are because of Rmath.
    
    * Handle some non-finite edge cases in gamma and binomial
    
    * Only switch to log-version of betalogcdf when the result is tiny
    
    * Handle the degenerate cases in beta(log)(c)cdf when alpha is zero
    
    * Handle the degenerate case of the gamma distribution when k==0.
    Also, only calculate the log(c)cdf based on the hypergeometric
    function when p is zero or subnormal
    
    * Address review comments
    
    * Avoid rational return type in gamma(log)(c)cdf
    
    * Handle the case when alpha and beta are zero in the betacdf

--- src/distrs/binom.jl
+++ src/distrs/binom.jl
@@ -1,17 +1,17 @@
 # functions related to binomial distribution
 
 # R implementations
-# For pdf and logpdf we use the Julia implementation
 using .RFunctions:
-    binomcdf,
-    binomccdf,
-    binomlogcdf,
-    binomlogccdf,
+    # binompdf,
+    # binomlogpdf,
+    # binomcdf,
+    # binomccdf,
+    # binomlogcdf,
+    # binomlogccdf,
     binominvcdf,
     binominvccdf,
     binominvlogcdf,
     binominvlogccdf
-
 
 # Julia implementations
 binompdf(n::Real, p::Real, k::Real) = exp(binomlogpdf(n, p, k))
@@ -21,4 +21,24 @@ function binomlogpdf(n::T, p::T, k::T) where {T<:Real}
     m = clamp(k, 0, n)
     val = min(0, betalogpdf(m + 1, n - m + 1, p) - log(n + 1))
     return 0 <= k <= n && isinteger(k) ? val : oftype(val, -Inf)
+end
+
+for l in ("", "log"), compl in (false, true)
+    fbinom = Symbol(string("binom", l, ifelse(compl, "c", "" ), "cdf"))
+    fbeta  = Symbol(string("beta" , l, ifelse(compl,  "", "c"), "cdf"))
+    @eval function ($fbinom)(n::Real, p::Real, k::Real)
+        if isnan(k)
+            return last(promote(n, p, k))
+        end
+        res = ($fbeta)(max(0, floor(k) + 1), max(0, n - floor(k)), p)
+
+        # When p == 1 the betaccdf doesn't return the correct result
+        # so these cases have to be special cased
+        if isone(p)
+            newres = oftype(res, $compl ? k < n : k >= n)
+            return $(l === "" ? :newres : :(log(newres)))
+        else
+            return res
+        end
+    end
 end
--- src/distrs/pois.jl
+++ src/distrs/pois.jl
@@ -1,12 +1,13 @@
 # functions related to Poisson distribution
 
 # R implementations
-# For pdf and logpdf we use the Julia implementation
 using .RFunctions:
-    poiscdf,
-    poisccdf,
-    poislogcdf,
-    poislogccdf,
+    # poispdf,
+    # poislogpdf,
+    # poiscdf,
+    # poisccdf,
+    # poislogcdf,
+    # poislogccdf,
     poisinvcdf,
     poisinvccdf,
     poisinvlogcdf,
@@ -20,3 +21,12 @@ function poislogpdf(λ::T, x::T) where {T <: Real}
     val = xlogy(x, λ) - λ - loggamma(x + 1)
     return x >= 0 && isinteger(x) ? val : oftype(val, -Inf)
 end
+
+# Just use the Gamma definitions
+poiscdf(λ::Real, x::Real) = gammaccdf(max(0, floor(x + 1)), 1, λ)
+
+poisccdf(λ::Real, x::Real) = gammacdf(max(0, floor(x + 1)), 1, λ)
+
+poislogcdf(λ::Real, x::Real) = gammalogccdf(max(0, floor(x + 1)), 1, λ)
+
+poislogccdf(λ::Real, x::Real) = gammalogcdf(max(0, floor(x + 1)), 1, λ)
