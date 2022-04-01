3f3df85e0ec38c48b4b2705e346fb9a09f7a3dfd
David Widmann <david.widmann@it.uu.se>
2022-04-01 16:26:17 +0200

    Fix test errors on Julia 1.0

--- test/rmath.jl
+++ test/rmath.jl
@@ -151,9 +151,15 @@ end
     # StatsFuns:   1.5205049885199752e-162
     # Rmath:       1.5205049885200616e-162
     # Mathematica: 1.520504988521358e-162
+    #
+    # With older SpecialFunctions + Julia versions a slightly larger tolerance is needed
     @testset "Beta(1000, 2)" begin
-        rmathcomp("beta", (1000, 2), setdiff(0.0:0.01:1.0, (0.58, 0.68)))
-        rmathcomp("beta", (1000, 2), [0.58, 0.68], 1e-12)
+        if VERSION < v"1.3"
+            rmathcomp("beta", (1000, 2), 0.0:0.01:1.0, 1e-12)
+        else
+            rmathcomp("beta", (1000, 2), setdiff(0.0:0.01:1.0, (0.58, 0.68)))
+            rmathcomp("beta", (1000, 2), [0.58, 0.68], 1e-12)
+        end
     end
 
     rmathcomp_tests("binom", [
