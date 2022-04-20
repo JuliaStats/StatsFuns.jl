3ae0b95235fcb8a9b392c636f370b6770365363b
Andreas Noack <andreas@noack.dk>
2022-04-20 09:12:30 +0200

    Use julia implementations for pdfs and some cdf-like functions (#113)
    * Use Julia implementations for (log)pdfs. Adjust Rmath tests to use
    testsets to give more informative errors.
    
    * Use Julia implementations for most cdf-like functions of the Chisq
    distribution.
    
    * Change Beta distribution to be using native implementations.
    
    * Switch binomial cdfs to use native implementations (but not the inverses)
    
    * Use native implementations for most cdf like gamma functions
    
    * Just use the gamma defitions for the chisq
    
    * Use the beta definitions for binomial
    
    * Add native implementions for Poisson's cdf-like functions
    
    * Rely on SpecialFunctions promotion handling in beta functions
    
    * Rely on gamma_inc_inv promotion in gammainv(c)cdf
    
    * Apply suggestions from code review
    
    * Add compat for HypergeometricFunctions
    
    * Handle negative arguments in Poisson (log)(c)cdf
    
    * Handle negative x in Gamma (log)(c)cdf
    
    * Handle arguments outside support for Beta and Fdist (log)(c)cdf
    
    * Fix Fdist quantile functions
    
    * Handle arguments out of support in Binomial (log)(c)cdf and apply
    the floor function to make the results correct for non-integer arguments
    
    * Avoid accidental promotion to Float64 in fdist(log)(c)cdf
    
    * Fix typo in "x" calculation for fdist(log)(c)cdf
    
    * Drop redundant low tolerance tests for Beta(1000, 2). We are now
    generally more accurate than Rmath so failure are because of Rmath.
    
    * Handle some non-finite edge cases in gamma and binomial
    
    * Only switch to log-version of betalogcdf when the result is tiny
    
    * Handle the degenerate cases in beta(log)(c)cdf when alpha is zero
    
    * Handle the degenerate case of the gamma distribution when k==0.
    Also, only calculate the log(c)cdf based on the hypergeometric
    function when p is zero or subnormal
    
    * Address review comments
    
    * Avoid rational return type in gamma(log)(c)cdf
    
    * Handle the case when alpha and beta are zero in the betacdf

--- src/distrs/fdist.jl
+++ src/distrs/fdist.jl
@@ -1,17 +1,5 @@
 # functions related to F distribution
 
-# R implementations
-# For pdf and logpdf we use the Julia implementation
-using .RFunctions:
-    fdistcdf,
-    fdistccdf,
-    fdistlogcdf,
-    fdistlogccdf,
-    fdistinvcdf,
-    fdistinvccdf,
-    fdistinvlogcdf,
-    fdistinvlogccdf
-
 # Julia implementations
 fdistpdf(ν1::Real, ν2::Real, x::Real) = exp(fdistlogpdf(ν1, ν2, x))
 
@@ -22,4 +10,20 @@ function fdistlogpdf(ν1::T, ν2::T, x::T) where {T<:Real}
     y = max(x, 0)
     val = (xlogy(ν1, ν1ν2) + xlogy(ν1 - 2, y) - xlogy(ν1 + ν2, 1 + ν1ν2 * y)) / 2 - logbeta(ν1 / 2, ν2 / 2)
     return x < 0 ? oftype(val, -Inf) : val
+end
+
+for f in ("cdf", "ccdf", "logcdf", "logccdf")
+    ff = Symbol("fdist"*f)
+    bf = Symbol("beta"*f)
+    @eval $ff(ν1::T, ν2::T, x::T) where {T<:Real} = $bf(ν1/2, ν2/2, inv(1 + ν2/(ν1*max(0, x))))
+    @eval $ff(ν1::Real, ν2::Real, x::Real) = $ff(promote(ν1, ν2, x)...)
+end
+for f in ("invcdf", "invccdf", "invlogcdf", "invlogccdf")
+    ff = Symbol("fdist"*f)
+    bf = Symbol("beta"*f)
+    @eval function $ff(ν1::T, ν2::T, y::T) where {T<:Real}
+        x = $bf(ν1/2, ν2/2, y)
+        return x/(1 - x)*ν2/ν1
+    end
+    @eval $ff(ν1::Real, ν2::Real, y::Real) = $ff(promote(ν1, ν2, y)...)
 end
