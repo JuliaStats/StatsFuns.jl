459f23fc092ce978969f5793b59475f6566a9f81
oscarddssmith <oscar.smith@juliahub.com>
2026-02-04 17:44:39 -0500

    add ntdistpdf

--- src/distrs/ntdist.jl
+++ src/distrs/ntdist.jl
@@ -2,8 +2,6 @@
 
 # R implementations
 using .RFunctions:
-    ntdistpdf,
-    ntdistlogpdf,
     ntdistcdf,
     ntdistccdf,
     ntdistlogcdf,
@@ -12,3 +10,19 @@ using .RFunctions:
     ntdistinvccdf,
     ntdistinvlogcdf,
     ntdistinvlogccdf
+
+using SpecialFunctions: gamma
+
+function ntdistpdf(k::Real, λ::Real, x::Real)
+    return exp(ntdistlogpdf(k, λ, x))
+end
+
+ntdistlogpdf(k::Real, λ::Real, x::Real) = ntdistlogpdf(promote(k, λ, x)...)
+
+function ntdistlogpdf(k::T, λ::T, x::T) where {T<:Real}
+    tdistlogpdf(k, x) - λ^2 / 2 +
+    log(_₁F₁((k + 1) / 2, 1 / 2, (λ * x)^2 / (2 * muladd(x,x,k))) +
+    sqrt(2) * λ * x * gamma(k/2 +1) / (sqrt(muladd(x,x,k))*gamma((k+1)/2)) * 
+        _₁F₁(k/2 + 1, 3/2, (λ * x)^2 / (2 * muladd(x,x,k)))
+    )
+end
