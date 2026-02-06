2930a6a94f96e90c663e6619443965999889979f
Oscar Smith <oscardssmith@gmail.com>
2026-02-05 22:20:53 -0500

    types

--- src/distrs/ntdist.jl
+++ src/distrs/ntdist.jl
@@ -20,9 +20,10 @@ end
 ntdistlogpdf(k::Real, λ::Real, x::Real) = ntdistlogpdf(promote(k, λ, x)...)
 
 function ntdistlogpdf(k::T, λ::T, x::T) where {T<:Real}
+    ONE = one(T)
+    Av = _₁F₁((k + 1) / 2, ONE / 2, (λ * x)^2 / (2 * muladd(x, x, k)))
+    Bv = sqrt(2*ONE) * λ * x * gamma(k/2 +1) / (sqrt(muladd(x, x, k)) * gamma((k+1)/2)) * 
+        _₁F₁(k/2 + 1, 3*ONE/2, (λ * x)^2 / (2 * muladd(x, x, k)))
     tdistlogpdf(k, x) - λ^2 / 2 +
-    log(_₁F₁((k + 1) / 2, 1 / 2, (λ * x)^2 / (2 * muladd(x,x,k))) +
-    sqrt(2) * λ * x * gamma(k/2 +1) / (sqrt(muladd(x,x,k))*gamma((k+1)/2)) * 
-        _₁F₁(k/2 + 1, 3/2, (λ * x)^2 / (2 * muladd(x,x,k)))
-    )
+    log(Av + Bv)
 end
