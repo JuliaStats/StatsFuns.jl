bf68d6e980849cca85f4634c93da5c344dd25b7f
Steven G. Johnson <stevenj@alum.mit.edu>
2026-06-09 11:10:11 -0400

    runic formatting

--- src/distrs/owens_t.jl
+++ src/distrs/owens_t.jl
@@ -12,29 +12,31 @@ using SpecialFunctions: erf, erfc
 using IrrationalConstants: inv2π, invsqrt2π
 
 # P(-∞<Z<=x)-0.5 with Z being normally distributed.
-owens_t_znorm1(x::Real) = erf(x*invsqrt2) / 2
+owens_t_znorm1(x::Real) = erf(x * invsqrt2) / 2
 
 # P(x<=Z<∞) with Z being normally distributed.
-owens_t_znorm2(x::Real) = erfc(x*invsqrt2) / 2
+owens_t_znorm2(x::Real) = erfc(x * invsqrt2) / 2
 
 # Auxiliary function, it computes an array key that is used to determine
 # the specific computation method for Owen's T and the order thereof
 # used in owens_t_dispatch.  Differs from C++ in using 1-based indices
-let hrange = ( 0.02f0, 0.06f0, 0.09f0, 0.125f0, 0.26f0, 0.4f0,  0.6f0,  1.6f0,  1.7f0,  2.33f0,  2.4f0,  3.36f0, 3.4f0,  4.8f0 ), # length 14
-    arange = (0.025f0, 0.09f0, 0.15f0, 0.36f0, 0.5f0, 0.9f0, 0.99999f0), # length 7
-    select = (1, 1, 2,13,13,13,13,13,13,13,13,16,16,16, 9,
-              1, 2, 2, 3, 3, 5, 5,14,14,15,15,16,16,16, 9,
-              2, 2, 3, 3, 3, 5, 5,15,15,15,15,16,16,16,10,
-              2, 2, 3, 5, 5, 5, 5, 7, 7,16,16,16,16,16,10,
-              2, 3, 3, 5, 5, 6, 6, 8, 8,17,17,17,12,12,11,
-              2, 3, 5, 5, 5, 6, 6, 8, 8,17,17,17,12,12,12,
-              2, 3, 4, 4, 6, 6, 8, 8,17,17,17,17,17,12,12,
-              2, 3, 4, 4, 6, 6,18,18,18,18,17,17,17,12,12) # 1-based indices (in 8x15 "matrix")
+let hrange = (0.02f0, 0.06f0, 0.09f0, 0.125f0, 0.26f0, 0.4f0, 0.6f0, 1.6f0, 1.7f0, 2.33f0, 2.4f0, 3.36f0, 3.4f0, 4.8f0), # length 14
+        arange = (0.025f0, 0.09f0, 0.15f0, 0.36f0, 0.5f0, 0.9f0, 0.99999f0), # length 7
+        select = (
+        1, 1, 2, 13, 13, 13, 13, 13, 13, 13, 13, 16, 16, 16, 9,
+        1, 2, 2, 3, 3, 5, 5, 14, 14, 15, 15, 16, 16, 16, 9,
+        2, 2, 3, 3, 3, 5, 5, 15, 15, 15, 15, 16, 16, 16, 10,
+        2, 2, 3, 5, 5, 5, 5, 7, 7, 16, 16, 16, 16, 16, 10,
+        2, 3, 3, 5, 5, 6, 6, 8, 8, 17, 17, 17, 12, 12, 11,
+        2, 3, 5, 5, 5, 6, 6, 8, 8, 17, 17, 17, 12, 12, 12,
+        2, 3, 4, 4, 6, 6, 8, 8, 17, 17, 17, 17, 17, 12, 12,
+        2, 3, 4, 4, 6, 6, 18, 18, 18, 18, 17, 17, 17, 12, 12,
+    ) # 1-based indices (in 8x15 "matrix")
     global owens_t_compute_code
     function owens_t_compute_code(h::Real, a::Real)
-        ihint = something(findfirst(>=(h), hrange), length(hrange)+1)
-        iaint = something(findfirst(>=(a), arange), length(arange)+1)
-        return select[(iaint-1)*15 + ihint]
+        ihint = something(findfirst(>=(h), hrange), length(hrange) + 1)
+        iaint = something(findfirst(>=(a), arange), length(arange) + 1)
+        return select[(iaint - 1) * 15 + ihint]
     end
 end
 
@@ -46,16 +48,16 @@ let ord = (2, 3, 4, 5, 7, 10, 12, 18, 10, 20, 30, 0, 4, 7, 8, 20, 0, 0) # 53-bit
 end
 
 # compute the value of Owen's T function with method T1 from the reference paper
-function owens_t_T1(h::T, a::T, m::Integer) where {T<:Real}
-    hs = -h*h/2
-    as = a*a
+function owens_t_T1(h::T, a::T, m::Integer) where {T <: Real}
+    hs = -h * h / 2
+    as = a * a
     aj = a * inv2π
     dj = expm1(hs)
-    gj = hs*exp(hs)
+    gj = hs * exp(hs)
     val = atan(a) * inv2π
     j, jj = one(m), T(1)
     while true
-        val += dj*aj/jj
+        val += dj * aj / jj
         m <= j && return val
         j += one(m)
         jj += 2
@@ -63,83 +65,93 @@ function owens_t_T1(h::T, a::T, m::Integer) where {T<:Real}
         dj = gj - dj
         gj *= hs / j
     end
+    return
 end
 
 # compute the value of Owen's T function with method T2 from the reference paper
-function owens_t_T2(h::T, a::T, m::Integer, ah::T) where {T<:Real}
-    maxii = m+m+one(m)
-    hs = h*h
-    as = -a*a
+function owens_t_T2(h::T, a::T, m::Integer, ah::T) where {T <: Real}
+    maxii = m + m + one(m)
+    hs = h * h
+    as = -a * a
     y = inv(hs)
     ii = one(m)
     val = zero(T)
-    vi = a * exp(-ah*ah/2) * invsqrt2π
+    vi = a * exp(-ah * ah / 2) * invsqrt2π
     z = owens_t_znorm1(ah) / h
     while true
-        val += z;
-        maxii <= ii && return val * exp(-hs/2) * invsqrt2π
+        val += z
+        maxii <= ii && return val * exp(-hs / 2) * invsqrt2π
         z = y * (vi - ii * z)
         vi *= as
         ii += oftype(ii, 2)
     end
+    return
 end
 
 # compute the value of Owen's T function with method T3 from the reference paper
-let c2 = (0.99999999999999987510, -0.99999999999988796462, 0.99999999998290743652,
-         -0.99999999896282500134, 0.99999996660459362918, -0.99999933986272476760,
-          0.99999125611136965852, -0.99991777624463387686, 0.99942835555870132569,
-         -0.99697311720723000295, 0.98751448037275303682, -0.95915857980572882813,
-          0.89246305511006708555, -0.76893425990463999675, 0.58893528468484693250,
-         -0.38380345160440256652, 0.20317601701045299653, -0.82813631607004984866E-01,
-          0.24167984735759576523E-01, -0.44676566663971825242E-02, 0.39141169402373836468E-03)
+let c2 = (
+        0.9999999999999998751, -0.99999999999988796462, 0.99999999998290743652,
+        -0.99999999896282500134, 0.99999996660459362918, -0.9999993398627247676,
+        0.99999125611136965852, -0.99991777624463387686, 0.99942835555870132569,
+        -0.99697311720723000295, 0.98751448037275303682, -0.95915857980572882813,
+        0.89246305511006708555, -0.76893425990463999675, 0.5889352846848469325,
+        -0.38380345160440256652, 0.20317601701045299653, -0.82813631607004984866e-1,
+        0.24167984735759576523e-1, -0.44676566663971825242e-2, 0.39141169402373836468e-3,
+    )
     global owens_t_T3
     function owens_t_T3(h::Float64, a::Float64, ah::Float64)
-        as = a*a
-        hs = h*h
+        as = a * a
+        hs = h * h
         y = inv(hs)
         ii = one(h)
-        vi = a * exp(-ah*ah/2) * invsqrt2π
-        zi = owens_t_znorm1(ah) / h;
+        vi = a * exp(-ah * ah / 2) * invsqrt2π
+        zi = owens_t_znorm1(ah) / h
         val = zero(h)
         i = 1
         while true
-            val += zi*c2[i];
-            i == lastindex(c2) && return val * exp(-hs/2) * invsqrt2π
+            val += zi * c2[i]
+            i == lastindex(c2) && return val * exp(-hs / 2) * invsqrt2π
             i += 1
-            zi = y * (ii*zi - vi)
+            zi = y * (ii * zi - vi)
             vi *= as
             ii += 2
         end
+        return
     end
 end
 
 # compute the value of Owen's T function with method T4 from the reference paper
-function owens_t_T4(h::T, a::T, m::Integer) where {T<:Real}
-    maxii = m+m+one(m)
-    hs = h*h
-    as = -a*a
+function owens_t_T4(h::T, a::T, m::Integer) where {T <: Real}
+    maxii = m + m + one(m)
+    hs = h * h
+    as = -a * a
     ii = 1
-    ai = a * exp(-hs*(1-as)/2) * inv2π
+    ai = a * exp(-hs * (1 - as) / 2) * inv2π
     yi = one(T)
     val = zero(T)
     while true
-        val += ai*yi
+        val += ai * yi
         maxii <= ii && return val
         ii += 2
-        yi = (1-hs*yi) / ii
+        yi = (1 - hs * yi) / ii
         ai *= as
     end
+    return
 end
 
 # compute the value of Owen's T function with method T5 from the reference paper
-let pts = (0.35082039676451715489E-02, 0.31279042338030753740E-01, 0.85266826283219451090E-01,
-           0.16245071730812277011, 0.25851196049125434828, 0.36807553840697533536,
-           0.48501092905604697475, 0.60277514152618576821, 0.71477884217753226516,
-           0.81475510988760098605, 0.89711029755948965867, 0.95723808085944261843, 0.99178832974629703586),
-    wts = (0.18831438115323502887E-01, 0.18567086243977649478E-01, 0.18042093461223385584E-01,
-           0.17263829606398753364E-01,  0.16243219975989856730E-01, 0.14994592034116704829E-01,
-           0.13535474469662088392E-01, 0.11886351605820165233E-01, 0.10070377242777431897E-01,
-           0.81130545742299586629E-02,  0.60419009528470238773E-02, 0.38862217010742057883E-02, 0.16793031084546090448E-02)
+let pts = (
+        0.35082039676451715489e-2, 0.3127904233803075374e-1, 0.8526682628321945109e-1,
+        0.16245071730812277011, 0.25851196049125434828, 0.36807553840697533536,
+        0.48501092905604697475, 0.60277514152618576821, 0.71477884217753226516,
+        0.81475510988760098605, 0.89711029755948965867, 0.95723808085944261843, 0.99178832974629703586,
+    ),
+        wts = (
+        0.18831438115323502887e-1, 0.18567086243977649478e-1, 0.18042093461223385584e-1,
+        0.17263829606398753364e-1, 0.1624321997598985673e-1, 0.14994592034116704829e-1,
+        0.13535474469662088392e-1, 0.11886351605820165233e-1, 0.10070377242777431897e-1,
+        0.81130545742299586629e-2, 0.60419009528470238773e-2, 0.38862217010742057883e-2, 0.16793031084546090448e-2,
+    )
     #=
        NOTICE:
        - The pts[] array contains the squares (!) of the abscissas, i.e. the roots of the Legendre
@@ -150,20 +162,20 @@ let pts = (0.35082039676451715489E-02, 0.31279042338030753740E-01, 0.85266826283
     =#
     global owens_t_T5
     function owens_t_T5(h::Float64, a::Float64)
-        as = a*a
-        hs = -h*h/2
+        as = a * a
+        hs = -h * h / 2
         r = 1 .+ as .* pts
         return sum(wts .* exp.(hs .* r) ./ r) * a
     end
 end
 
 # compute the value of Owen's T function with method T6 from the reference paper
-function owens_t_T6(h::T, a::T) where {T<:Float64}
-    normh = owens_t_znorm2(h);
+function owens_t_T6(h::T, a::T) where {T <: Float64}
+    normh = owens_t_znorm2(h)
     y = 1 - a
     r = atan(y, 1 + a)
     val = normh * (1 - normh) / 2
-    !iszero(r) && (val -= r * exp(-y*h*h/2r) * inv2π)
+    !iszero(r) && (val -= r * exp(-y * h * h / 2r) * inv2π)
     return val
 end
 
@@ -175,10 +187,10 @@ function owens_t_dispatch(h::Float64, a::Float64, ah::Float64)
     # page 1077 of Owen's original paper:
     iszero(h) && return atan(a) * inv2π
     iszero(a) && return zero(h)
-    a == 1 && return owens_t_znorm2(-h) * owens_t_znorm2(h) / 2;
+    a == 1 && return owens_t_znorm2(-h) * owens_t_znorm2(h) / 2
     @assert a <= 1 # when a>1 we call this routine with 1/a:
 
-    icode = owens_t_compute_code(h, a);
+    icode = owens_t_compute_code(h, a)
     m = owens_t_get_order(icode, typeof(h))
     meth = (1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 3, 4, 4, 4, 4, 5, 6) # 18 entries
     method = meth[icode]
@@ -189,7 +201,7 @@ function owens_t_dispatch(h::Float64, a::Float64, ah::Float64)
     method == 3 && return owens_t_T3(h, a, ah)
     method == 4 && return owens_t_T4(h, a, m)
     method == 5 && return owens_t_T5(h, a)
-    method == 6 && return owens_t_T6(h, a)
+    return method == 6 && return owens_t_T6(h, a)
 end
 
 # compute Owen's T function, T(h,a), for arbitrary values of h and a
@@ -200,19 +212,19 @@ function _owens_t(h::Float64, a::Float64)
     # Use equation (2) in the paper to remap the arguments
     # such that h>=0 and 0<=a<=1 for the call of the actual
     # computation routine.
-    abs_a = abs(a);
-    abs_ah = abs_a*h;
+    abs_a = abs(a)
+    abs_ah = abs_a * h
 
     val = if abs_a <= 1
         owens_t_dispatch(h, abs_a, abs_ah)
     elseif h <= oftype(h, 0.67)
-        normh = owens_t_znorm1(h);
-        normah = owens_t_znorm1(abs_ah);
-        1//4 - normh*normah - owens_t_dispatch(abs_ah, inv(abs_a), h)
+        normh = owens_t_znorm1(h)
+        normah = owens_t_znorm1(abs_ah)
+        1 // 4 - normh * normah - owens_t_dispatch(abs_ah, inv(abs_a), h)
     else
-        normh = owens_t_znorm2(h);
-        normah = owens_t_znorm2(abs_ah);
-        (normh+normah)/2 - normh*normah - owens_t_dispatch(abs_ah, inv(abs_a), h)
+        normh = owens_t_znorm2(h)
+        normah = owens_t_znorm2(abs_ah)
+        (normh + normah) / 2 - normh * normah - owens_t_dispatch(abs_ah, inv(abs_a), h)
     end
     return a < 0 ? -val : val # exploit that T(h,-a) == -T(h,a)
 end
